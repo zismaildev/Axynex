8f4822960f9d17bdc66e44bba9cc7781ea817c5e
zismaildev <nattapong130247@gmail.com>
2025-05-24 15:37:53 +0700

    fix navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -78,6 +78,7 @@ export const Navbar = () => {
             as={Link}
             className="text-sm font-normal text-default-600 bg-default-100"
             href={siteConfig.links.sponsor}
+            startContent={<HeartFilledIcon className="text-danger" />}
             variant="flat"
           >
             Sing in
@@ -98,7 +99,6 @@ export const Navbar = () => {
             as={Link}
             className="text-sm font-normal text-default-600 bg-default-100"
             href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
             variant="flat"
           >
             Sing in
